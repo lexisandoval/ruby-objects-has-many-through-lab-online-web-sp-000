f04fd7085c4b2c1c9590ef8e433fdd359729d0a6
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-02-04 16:52:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,21 @@
-class Song
+class Genre
 
+  @@all = []
+  attr_accessor :name, :artist, :genre
 
+  def initialize(name)
+    @name = name
+    @artist = artist
+    @genre = genre
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 
 end
