8510ad8eea353b85f28ab092a76aac834e3cf0ba
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-02-04 17:03:25 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,31 @@
+class Appointment
+
+  @@all = []
+  attr_accessor :date, :patient, :doctor
+
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  # def appointments
+  #   Appointments.all.select do |appt|
+  #     appt.doctor == self
+  #   end
+  # end
+  #
+  # def new_appointment(date, patient)
+  #   Appointment.new(date, patient, self)
+  # end
+
+end
