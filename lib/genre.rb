ec36d9217fe946d57a1e7525d40ec05710c437f0
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-02-04 16:50:34 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,19 @@
+class Genre
+
+  @@all = []
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
